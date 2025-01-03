5142bd965ec1a830bf624ca5013ee92392c1e457
SuperTanner1 <iqbalabdullah39@gmail.com>
2025-01-03 10:38:22 +0000

    created the remove_rows_from_df function

--- functions.py
+++ functions.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy as sp
 
+dataset1 = {'index1': ['a', 'b', 'c', 'd'], 'index2': ['aa', 'bb', 'cc', 'dd'], 'value_x': [10, 22, 56, 17]}
+dataset2 = {'index1': ['a', 'b', 'c', 'd'], 'index2': ['aa', 'bb', 'cc', 'dd'], 'value_y': [15, 30, 74, 23]}
+
+df1 = pd.DataFrame(dataset1)
+df2 = pd.DataFrame(dataset2)
+
 def merge_datasets(df, df1, relatedColumn):
     mergedDataset = pd.merge(df, df1, how='inner', on=relatedColumn)
     cleanedDataset = mergedDataset.transpose().drop_duplicates().transpose()
@@ -12,7 +18,10 @@ def create_model(x, y, degree):
     return np.polyfit(x, y, degree)
 def calculate_correlation_columns(series, series1):
     pass
-def remove_values_from_column(df, column, values):
-    pass
+def remove_rows_from_df(df, column, values):
+    if type(values) != list:
+        return df1[np.invert(df1[column] == values)]
+    else:
+        return df1[np.invert(df1[column].isin(values))]
 def replace_values_in_column(df):
     pass
